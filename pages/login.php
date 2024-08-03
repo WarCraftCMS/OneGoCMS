16e141efe532bcfa911ff8c136399337a4027d5b
Indra <66976753+Shirokosmiles@users.noreply.github.com>
2024-08-04 05:54:18 +0600

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -4,7 +4,7 @@
        //$login->login_checks();
    
        if ($login->login()) {
-           header("Location: ?page=home");
+           header("Location: ?page=account");
            exit();
        }
    }
@@ -24,41 +24,39 @@
         <div class="container">
             <div class="max-w-full mt-36 2xl:pt-0">
                 <h1 class="mb-5 text-4xl font-bold text-white text-shadow_dark">
-                    Login to your account
+                    Войдите в свою учетную запись
                 </h1>
                 <div class="text-white bg-slate-950/60 p-9 rounded-lg text-left leading-loose">
                 <form class="form" method="POST">  
                         <input type="hidden" name="xc_cv2" value="db9d5fca33cfe05e67440626abc4d5ee">                    <div>
                         <label class="input input-bordered flex items-center gap-2 bg-zinc-900/40">
-                            <span class="mr-2 text-center"><i class="fa-regular fa-user mr-1"></i> Username</span>
+                            <span class="mr-2 text-center">Логин</span>
                             <input type="text" name="username" id="username" class="grow w-48 md:w-auto" placeholder="Введите Логин" />
                         </label>
                     </div>
                     <div class="mt-2">
                         <label class="input input-bordered flex items-center gap-2 bg-zinc-900/40">
-                            <span class="mr-2 text-center"><i class="fa-regular fa-key mr-1"></i> Password</span>
+                            <span class="mr-2 text-center">Пароль</span>
                             <input type="password" name="password" class="grow w-48 md:w-auto" placeholder="Введите свой пароль" />
                         </label>
                     </div>
-                    <div class="mx-auto text-center mt-2 items-center">
-                        <div class="cf-turnstile mx-auto" data-sitekey="0x4AAAAAAAaFmg0qOjUrGNSk"></div>                    </div>
                     <div class="mx-auto text-center">
                         <button type="submit" name="submit" class="btn mx-auto bg-blue-500/70 hover:bg-blue-700 text-white mt-2">
                             Авторизоваться
                         </button>
                     </div>
-                    <div style="display:none"><label>What is your code?</label><input type="text" name="code_v2" value=""></div></form>
+                    </form>
                 </div>
                 <div class="divider text-center mt-5">
                     OR
                 </div>
                 <div class="mx-auto text-center mt-5">
-                    <a href="https://masterwow.net/sign-up" class="btn bg-teal-500/70 hover:bg-teal-700 text-white">
-                        Register
+                    <a href="?page=register" class="btn bg-teal-500/70 hover:bg-teal-700 text-white">
+                        Регистрация
                     </a>
-                    <a href="https://masterwow.net/restore-password"
+                    <a href="?page=login"
                         class="btn bg-rose-500/70 hover:bg-rose-700 text-white">
-                        Forgot Password
+                        Вход
                     </a>
                 </div>
             </div>
