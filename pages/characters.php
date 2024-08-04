8a25c222c85f24ee63360ee4f77c2ba29010a891
Indra <66976753+Shirokosmiles@users.noreply.github.com>
2024-08-04 19:33:29 +0600

    Update characters.php

--- pages/characters.php
+++ pages/characters.php
@@ -3,30 +3,8 @@ $global->check_logged_in();
 $account = new Account($_SESSION['username']);
 
 if (isset($_POST['change_password'])) {
-    header("Location: ?page=changepassword");
-    exit();
-}
-
-$message = null;
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['teleport_character'])) {
-    $account_id = intval($_POST['account_id']);
-    $guid = intval($_POST['guid']);
-
-    $character = new Character();
-    $result = $character->teleportCharacter($account_id, $guid);
-
-    switch($result) {
-        case true:
-            $message = "Персонаж успешно телепортировался.";
-            break;
-        case false:
-            $message = "Телепортация не удалась. Пожалуйста, попробуйте снова.";
-            break;
-        default:
-            $message = "Произошла ошибка.";
-            break;
-    }
+   header("Location: ?page=changepassword");
+   exit();
 }
 ?>
     <div class="hero min-h-screen hero13">
@@ -60,51 +38,48 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['teleport_character']))
             foreach ($characters as $character) {
             ?><tbody>
                                 <tr>
-                                    <td class='text-center'>
-                                        <span><font color="<?= $character['class_color']; ?>"><?= $character['name']; ?></font></span>
-                                    </td>
-                                    <td class='text-center text-white sm:block hidden'>
-                                        <div class="tooltip" data-tip="<?= $character['faction']; ?> - <?= $character['race']; ?> - <?= $character['class']; ?>">
+                                        <td class='text-center'>
+                                            <span><font color="<?= $character['class_color']; ?>"><?= $character['name']; ?></font></span>
+                                        </td>
+                                        <td class='text-center text-white sm:block hidden'>
+                                            <div class="tooltip" data-tip="<?= $character['faction_text']; ?> - <?= $character['class_name']; ?> - <?= $character['race_name']; ?>">
                                             <img class="h-5 inline" src="<?= $character['faction']; ?>" />
                                             <img class="h-5 inline rounded-full" src="<?= $character['class_image']; ?>">
                                             <img class="h-5 inline rounded-full" src="<?= $character['race_image']; ?>" class="mr-2">
-                                        </div>
-                                    </td>
-                                    <td class='text-center'>
-                                        <span><?= $character['level']; ?></span>
-                                    </td>
-                                    <td class='text-center'>
-                                        <span><?= $character['guild_name']; ?></span>
-                                    </td>
-                                    <td class='text-center'>
-                                        <span><?= $character['gold']; ?> <img class="h-5 inline rounded-full" src="<?= $character['gold_image']; ?>" class="mr-2">
+                                            </div>
+                                        </td>
+                                        <td class='text-center'>
+                                            <span><?= $character['level']; ?></span>
+                                        </td>
+                                        <td class='text-center'>
+                                            <span><?= $character['guild_name']; ?></span>
+                                        </td>
+                                        <td class='text-center'>
+                                            <span><?= $character['gold']; ?> <img class="h-5 inline rounded-full" src="<?= $character['gold_image']; ?>" class="mr-2">
                                             <?= $character['silver']; ?> <img class="h-5 inline rounded-full" src="<?= $character['silver_image']; ?>" class="mr-2"> 
                                             <?= $character['copper']; ?> <img class="h-5 inline rounded-full" src="<?= $character['copper_image']; ?>" class="mr-2"></span>
-                                    </td>
-                                    <td class='text-center'>
-                                        <span><img class="h-5 inline rounded-full" src="<?= $character['arena_image']; ?>" class="mr-2"> <?= $character['arenaPoints']; ?></span>
-                                    </td>
-                                    <td class='text-center'>
-                                        <span><img class="h-5 inline rounded-full" src="<?= $character['honor_image']; ?>" class="mr-2"> <?= $character['totalHonorPoints']; ?></span>
-                                    </td>
-                                    <td class='text-center'>
-                                        <span><img class="h-5 inline rounded-full" src="" class="mr-2"> </span>
-                                    </td>
-                                    <td class='text-center'>
-                                        <span><img class="h-5 inline rounded-full" src="<?= $character['achievement_image']; ?>" class="mr-2"> <?= $character['achievement_count']; ?></span>
-                                    </td>
-                                    <td>
-                                        <form method="post">
-                                            <input type="hidden" name="account_id" value="<?= $_SESSION['account_id']; ?>">
-                                            <input type="hidden" name="guid" value="<?= $character['guid']; ?>">
-                                            <button type="submit" class="btn bg-rose-900 hover:bg-rose-700 text-white btn-sm">
+                                        </td>
+                                        <td class='text-center'>
+                                            <span><img class="h-5 inline rounded-full" src="<?= $character['arena_image']; ?>" class="mr-2"> <?= $character['arenaPoints']; ?></span>
+                                        </td>
+                                        <td class='text-center'>
+                                            <span><img class="h-5 inline rounded-full" src="<?= $character['honor_image']; ?>" class="mr-2"> <?= $character['totalHonorPoints']; ?></span>
+                                        </td>
+                                        <td class='text-center'>
+                                            <span><img class="h-5 inline rounded-full" src="" class="mr-2"> </span>
+                                        </td>
+                                        <td class='text-center'>
+                                            <span><img class="h-5 inline rounded-full" src="<?= $character['achievement_image']; ?>" class="mr-2"> <?= $character['achievement_count']; ?></span>
+                                        </td>
+
+                                        <td>
+                                            <button onclick="" class="btn bg-rose-900 hover:bg-rose-700 text-white btn-sm">
                                                 Застрял
                                             </button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            </tbody>
-                            <?php } ?>   
+                                            </td>
+                                    </tr>
+
+                            </tbody><?php } ?>   
                         </table>
                     </div>
                 </div>
