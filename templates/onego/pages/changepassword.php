0d8a26263af8d55d36ebf4de90dfd03e51eb9382
Null <107396951+a-wow@users.noreply.github.com>
2025-06-21 21:23:14 +0300

    Update changepassword.php

--- templates/onego/pages/changepassword.php
+++ templates/onego/pages/changepassword.php
@@ -1,17 +1,15 @@
 <?php
 $global->check_logged_in();
 $account = new Account($_SESSION['username']);
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if ($_POST['new_password'] == $_POST['confirm_password']){  // check if new password matches confirmed password
-        $change_password_status = $account->change_password($_POST['old_password'], $_POST['new_password']);
-        if ($change_password_status == "success") {
-            echo '<div class="alert alert-success">Password has been successfully changed.</div>';
-        } else {
-            echo '<div class="alert alert-danger">Failed to change password. Please make sure your old password is correct.</div>';
-        }
-    } else {
-        echo '<div class="alert alert-danger">New password and confirmed password do not match.</div>';
-    }
+
+if (isset($_SESSION['success_message'])) {
+    echo '<div class="alert alert-success">' . htmlspecialchars($_SESSION['success_message']) . '</div>';
+    unset($_SESSION['success_message']);
+}
+
+if (isset($_SESSION['error'])) {
+    echo '<div class="alert alert-danger">' . htmlspecialchars($_SESSION['error']) . '</div>';
+    unset($_SESSION['error']);
 }
 ?>
    <div class="hero min-h-screen hero12">
